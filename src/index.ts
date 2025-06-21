6cd0af158c8077065a87f91ca534260dcf2232b9
Pedro Moreira <pedsmoreira@gmail.com>
2025-06-21 20:45:43 -0300

    export types and functions for debugging

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,40 @@
+// Export utils
+export { type ExpandObject } from "./types/object-type-utils";
+
+// Export data type
+export {
+  type DataType,
+  type ValidDataType,
+  type TruthyValue,
+  truthyValues,
+  type FalsyValue,
+  falsyValues,
+  castData,
+  type CastData,
+  dataTypes,
+  isValidDataType,
+  type DataTypeByName,
+} from "./data-type";
+
+// Export arg, flag and blueprint
+export { type Arg, type ParseArg, parseArg, castArg, formatArg } from "./arg";
+
+export {
+  type Flag,
+  type ParseFlag,
+  parseFlag,
+  castFlag,
+  formatFlag,
+} from "./flag";
+
+export {
+  type Blueprint,
+  type RecordFromBlueprint,
+  type ParseBlueprint,
+} from "./blueprint";
+
+// Export command
 export { command } from "./command";
+
+// Export kit
 export { prompt } from "./kit/prompt";
