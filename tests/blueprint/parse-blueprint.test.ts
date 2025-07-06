3d4fc0e75ec3e30c8552a28e80002e57c75d5d94
Pedro Moreira <pedsmoreira@gmail.com>
2025-07-06 13:14:38 -0300

    Implement missing blueprint test

--- tests/blueprint/parse-blueprint.test.ts
+++ tests/blueprint/parse-blueprint.test.ts
@@ -1,28 +1,23 @@
-import { expect, test } from "vitest";
+import { test } from "vitest";
 
-import {
-  type ParseBlueprint,
-  parseBlueprint,
-} from "../../src/blueprint/parse-blueprint";
+import { createParseBlueprintMatcher } from "./utils/create-parse-blueprint-matcher";
 
 // Note: blueprint tests are only to assert that the integration with `parseArg`
 //       and `parseFlag` are working. Specific arg and flag cases are covered
 //       by the respective test suites.
 
 test("just the name", () => {
-  const blueprintToken = "foo";
-
-  expect(parseBlueprint(blueprintToken)).toEqual({
+  const { expectParseBlueprintMatch } = createParseBlueprintMatcher({
     __objectType__: "blueprint",
     name: "foo",
     parts: [] as never,
-  } satisfies ParseBlueprint<typeof blueprintToken>);
+  } as const);
+
+  expectParseBlueprintMatch("foo");
 });
 
 test("with arg but no flag", () => {
-  const blueprintToken = "foo arg_one";
-
-  expect(parseBlueprint(blueprintToken)).toEqual({
+  const { expectParseBlueprintMatch } = createParseBlueprintMatcher({
     __objectType__: "blueprint",
     name: "foo",
     parts: [
@@ -35,13 +30,13 @@ test("with arg but no flag", () => {
         fallback: null,
       },
     ],
-  } satisfies ParseBlueprint<typeof blueprintToken>);
+  } as const);
+
+  expectParseBlueprintMatch("foo arg_one");
 });
 
 test("with flag but no arg", () => {
-  const blueprintToken = "foo --flag_one(-f):number";
-
-  expect(parseBlueprint(blueprintToken)).toEqual({
+  const { expectParseBlueprintMatch } = createParseBlueprintMatcher({
     __objectType__: "blueprint",
     name: "foo",
     parts: [
@@ -55,11 +50,54 @@ test("with flag but no arg", () => {
         fallback: null,
       },
     ],
-  } satisfies ParseBlueprint<typeof blueprintToken>);
+  } as const);
+
+  expectParseBlueprintMatch("foo --flag_one(-f):number");
 });
 
-test.todo("with multiple args and flags", () => {});
+test("with multiple args and flags", () => {
+  const { expectParseBlueprintMatch } = createParseBlueprintMatcher({
+    __objectType__: "blueprint",
+    name: "model:new",
+    parts: [
+      {
+        __objectType__: "arg",
+        name: "name",
+        type: "string",
+        explicitType: false,
+        optional: false,
+        fallback: null,
+      },
+      {
+        __objectType__: "arg",
+        name: "domain",
+        type: "string",
+        explicitType: false,
+        optional: true,
+        fallback: null,
+      },
+      {
+        __objectType__: "flag",
+        name: "form",
+        short: null,
+        type: "boolean",
+        explicitType: false,
+        required: false,
+        fallback: true,
+      },
+      {
+        __objectType__: "flag",
+        name: "prefix",
+        short: "p",
+        type: "string",
+        explicitType: true,
+        required: false,
+        fallback: null,
+      },
+    ],
+  } as const);
 
-test.todo("with stray - in the string", () => {});
-
-test.todo("with stray -- in the string", () => {});
+  expectParseBlueprintMatch(
+    "model:new name domain? --form=true --prefix(-p):string",
+  );
+});
---
+++ tests/blueprint/utils/create-parse-blueprint-matcher.ts
@@ -0,0 +1,38 @@
+import { expect } from "vitest";
+
+import { type Blueprint } from "../../../src/blueprint/blueprint";
+import {
+  type ParseBlueprint,
+  parseBlueprint,
+} from "../../../src/blueprint/parse-blueprint";
+import { skipCustomFunctionInStackTrace } from "../../utils/test-error-utils";
+
+// Ensure the blueprint is passed with `as const` to avoid attribute types being too broad
+type IsLiteralBlueprint<B extends Blueprint> = B["name"] extends `${infer _}`
+  ? B
+  : never;
+
+/**
+ * Utility for testing `parseBlueprint`.
+ *
+ * It returns a function that can be used to assert multiple times the
+ * given `blueprintToken` returns proper values and types.
+ */
+export function createParseBlueprintMatcher<B extends Blueprint>(
+  expected: B & IsLiteralBlueprint<B>,
+) {
+  const expectParseBlueprintMatch = <BlueprintToken extends string>(
+    blueprintToken: BlueprintToken &
+      (ParseBlueprint<BlueprintToken> extends B ? unknown : never),
+  ) => {
+    try {
+      expect(parseBlueprint(blueprintToken)).toEqual(expected);
+    } catch (error) {
+      throw error instanceof Error
+        ? skipCustomFunctionInStackTrace(error, "expectParseBlueprintMatch")
+        : error;
+    }
+  };
+
+  return { expectParseBlueprintMatch };
+}
